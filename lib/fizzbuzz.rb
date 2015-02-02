f1c1abf75296d38bb3d4f398e3fbe9af790af860
Mishal Islam <mishal.islam01@gmail.com>
2015-02-02 11:48:52 +0000

    fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,22 +1,22 @@
 def divisible_by_three(number)
-	divisible(number, 3)
+  divisible_by(number, 3)
 end
 
 def divisible_by_five(number)
-	divisible(number, 5)
+  divisible_by(number, 5)
 end
 
 def divisible_by_fifteen(number)
-	divisible(number, 15)
+  divisible_by(number, 15)
 end
 
-def divisible(number, divisor)
-	number % divisor === 0
+def divisible_by(number, divisor)
+  number % divisor === 0
 end
 
 def fizzbuzz_says(number)
-	return "fizzbuzz" if divisible_by_fifteen(number)
-	return "buzz" if divisible_by_five(number)
-	return "fizz" if divisible_by_three(number)
-	number
+  return 'fizzbuzz' if divisible_by_fifteen(number)
+  return 'buzz' if divisible_by_five(number)
+  return 'fizz' if divisible_by_three(number)
+  number
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,62 +1,57 @@
 require 'fizzbuzz'
 
-describe 'fizzbuz' do
-	
-	context "it should know when a number is" do
+describe 'fizzbuzz' do
 
-		it "divisible by three" do
+	context 'should know when a number is' do
+	
+		it 'divisible by three' do
 			expect(divisible_by_three(3)).to be true
 		end
 
-		it "divisible by five" do
+		it 'divisible by five' do
 			expect(divisible_by_five(5)).to be true
 		end
 
-		it "divisible by fifteen" do
+		it 'divisible by fifteen' do
 			expect(divisible_by_fifteen(15)).to be true
 		end
 
 	end
 
-	context "it should know when a number is NOT" do
+	context 'should know when a number is not' do
 
-		it "divisible by three" do
+		it 'divisible by three' do
 			expect(divisible_by_three(1)).to be false
 		end
 
-		it "divisible by five" do
+		it 'divisible by five' do
 			expect(divisible_by_five(1)).to be false
 		end
 
-		it "divisible by fifteen" do
+		it 'divisible by fifteen' do
 			expect(divisible_by_fifteen(1)).to be false
 		end
 
 	end
 
-	context "fizzbuzz says" do
+	context 'should say' do
 
-		it "should say fizz when divisible by three" do
-			expect(fizzbuzz_says(3)).to eq("fizz")
+		it '"fizz" if the number is divisible by three' do
+			expect(fizzbuzz_says(3)).to eq 'fizz'
 		end
 
-		it "should say buzz when divisible by five" do
-			expect(fizzbuzz_says(5)).to eq("buzz")
+		it '"buzz" if the number is divisible by five' do
+			expect(fizzbuzz_says(5)).to eq 'buzz'
 		end
 
-		it "should say fizzbuzz when divisible by fifteen" do
-			expect(fizzbuzz_says(15)).to eq("fizzbuzz")
+		it '"fizzbuzz" if the number is divisible by fifteen' do
+			expect(fizzbuzz_says(15)).to eq 'fizzbuzz'
 		end
 
-		it "should say number if it is not divisible by three, five or fifteen" do
-			expect(fizzbuzz_says(1)).to eq(1)
+		it 'number if the number is not divisible by three, five or fifteen' do
+			expect(fizzbuzz_says(1)).to eq 1
 		end
 
 	end
 
 end
-
-
-
-
-
